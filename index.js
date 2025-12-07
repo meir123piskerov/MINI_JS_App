ba93af87213a6b74a8e63fa3d469a10e210554a2
Ari Durlacher <arikodcode@gmail.com>
2025-12-07 09:33:22 +0200

    Welcome, Agents

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { THEME_COLOR } from "./config.js";
+import { THEME_COLOR } from './config.js';
 
 function getHeroTitle() {
-  return "Welcome";
+  return 'Welcome, Agents';
 }
 
-console.log("HERO TITLE:", getHeroTitle());
-console.log("THEME COLOR:", THEME_COLOR);
+console.log('HERO TITLE:', getHeroTitle());
+console.log('THEME COLOR:', THEME_COLOR);
